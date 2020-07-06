445c40ca3ac0c08306d7a2c6040be3a211272a6e
Jasio <mcyanek@gmail.com>
2020-07-06 11:31:27 +0200

    adds generic permutation of pawns position depending on color

--- src/resources/js/game.js
+++ src/resources/js/game.js
@@ -2,6 +2,18 @@ function Player(color){
     this.color = color;
 }
 
+function Pawn(x, y, type){
+    placeOnBoard = function (Player) {
+        if (Player.color === 'white'){
+            this.x = x;
+            this.y = y;
+        } else {
+            this.x = 8-x;
+            this.y = 8-y;
+        }
+    }
+}
+
 Players = [];
 Players.add(new Player(black));
 Players.add(new Player(white));
@@ -11,7 +23,7 @@ Figures = [];
 function initialSetup(){
     for (Player of Players){
         for (Figure of Figures){
-            Figure.placeOnBoard();
+            Figure.placeOnBoard(Player);
         }
     }
 }
