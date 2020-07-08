2b14d890a3ac0e07f3c35782c3455dedb3140fa2
Dominik <domek99@gmail.com>
2020-07-08 10:52:49 +0200

    add basic methods for highlights

--- src/resources/js/highlights.js
+++ src/resources/js/highlights.js
@@ -1,27 +1,49 @@
 function highlightOn(img) {
-    img.parentElement.style.background = "green";
+    img.parentElement.className = "highlighted";
     let parentId = img.parentElement.id;
     parentId = parentId.split("");
     let j = parentId[0].charCodeAt(0) - 96;
     let i = parentId[1];
     console.log(i, j);
-    for (let b = j; b < j+2; b++){
-    console.log(b);
-        let id = String.fromCharCode(b+96) + i.toString();
-        document.getElementById(id).style.background = "green";
+    console.log(Board[i][j]);
+    let moves;
+    let figure = Board[i][j].occupyingFigure;
+    switch(figure.type) {
+        case "rook":
+            moves = rookMoves("white", i, j);
+            break;
+        case "pawn":
+            moves = pawnMoves("white", i, j);
+            break;
+        case "bishop":
+            moves = bishopMoves("white", i, j);
+            break;
+        case "queen":
+            moves = queenMoves("white", i, j);
+            break;
+        case "knight":
+            moves = knightMoves("white", i, j);
+            break;
     }
+    console.log(moves);
+    for (element of moves) {
+        console.log(element);
+
+    }
+//    for (let b = j; b < j+2; b++){
+//        let id = String.fromCharCode(b+96) + i.toString();
+//        document.getElementById(id).className = "highlighted";
+//    }
 }
 
 function highlightOff(img) {
-    img.parentElement.removeAttribute("style");
+    img.parentElement.className = "field";
         let parentId = img.parentElement.id;
         parentId = parentId.split("");
         let j = parentId[0].charCodeAt(0) - 96;
         let i = parentId[1];
-        console.log(i, j);
         for (let b = j; b < j+2; b++){
-        console.log(b);
             let id = String.fromCharCode(b+96) + i.toString();
-            document.getElementById(id).removeAttribute("style");
+            document.getElementById(id).className = "field";
         }
 }
