ebdee7bd36238090602be506ee3cabc0a1f59cbb
Jasio <mcyanek@gmail.com>
2020-07-08 23:38:31 +0200

    implements checking checks, and king moves excluding castling

--- src/resources/js/game.js
+++ src/resources/js/game.js
@@ -29,6 +29,7 @@ function FigureOnBoard(type, color){
             case 'bishop': bishopMoves(Player);
             case 'queen': queenMoves(Player);
             case 'knight': knightMoves(Player);
+            case 'king': kingMoves(Player);
         }
     }
 }
@@ -140,7 +141,72 @@ function queenMoves(Player, i, j){
 function kingMoves(Player, i, j){
     let moves = [];
     let directions = [[1, 1], [-1, 1], [-1, -1], [1, -1], [1, 0], [-1, 0], [0, 1],[0, -1]];
+    for (direction of direstions){
+        if (!isChecked(Player, i+direction[0], j+direction[1]) && Board[i+direction[0]][j+direction[1]].occupyingFigure.color !== Player){
+            moves.push(Board[i+direction[0]][j+direction[1]]);
+        }
+    }
     return moves;
+}
+
+function isChecked(Player, i, j){
+    if (Player === 'white') {
+        let op = 'black';
+        let opDirection = -1;
+    }
+    else {
+        let op = 'white';
+        let opDirection = 1;
+    }
+
+    for (direction of [[1, 1], [-1, 1], [-1, -1], [1, -1]]) {
+        let ii = i + direction[0];
+        let jj = j + direction[1];
+        while (ii > 0 && jj > 0 && ii < 9 && jj < 9) {
+            if (Board[ii][jj].occupyingFigure.color === Player) {
+                break;
+            } else if (Board[ii][jj].occupyingFigure.color === op) {
+                if (Board[ii][jj].occupyingFigure.type === 'queen' ||
+                    Board[ii][jj].occupyingFigure.type === 'bishop') {
+                    return true;
+                } else break;
+            }
+            ii += direction[0];
+            jj += direction[1];
+        }
+    }
+
+    for (direction of  [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
+        let ii = i + direction[0];
+        let jj = j + direction[1];
+        while (ii > 0 && jj > 0 && ii < 9 && jj < 9) {
+            if (Board[ii][jj].occupyingFigure.color === Player) {
+                break;
+            } else if (Board[ii][jj].occupyingFigure.color === op) {
+                if (Board[ii][jj].occupyingFigure.type === 'queen' ||
+                    Board[ii][jj].occupyingFigure.type === 'rook') {
+                    return true;
+                } else break;
+            }
+            ii += direction[0];
+            jj += direction[1];
+        }
+    }
+
+    for (direction of [[2, 1], [1, 2], [-1, -2], [-2, -1], [2, -1], [-2, 1], [-1, 2], [1, -2]]){
+        if (i+direction[0] < 9 && i+direction[0] > 0 && j+direction[1] > 0 && j+direction[1] < 9 &&
+            Board[i+direction[0]][j+direction[1]].occupyingFigure.type === 'knight' &&
+            Board[i+direction[0]][j+direction[1]].occupyingFigure.color === op) return true;
+    }
+
+    for (direction of [[-1,opDirection],[1,opDirection]]){
+            if (Board[i+direction[0]][j+direction[1]].occupyingFigure.type === 'pawn' &&
+                Board[i+direction[0]][j+direction[1]].occupyingFigure.color === op){
+                return true
+            }
+        }
+    
+    return false;
 }
 
 function Pawn(x, y, type){
