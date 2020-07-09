fcfaf99e1fddc8667df80d7f14796600c3f0bfcb
Jasio <mcyanek@gmail.com>
2020-07-09 11:33:08 +0200

    removes redundant code in behavioral patterns

--- src/resources/js/game.js
+++ src/resources/js/game.js
@@ -1,7 +1,28 @@
+function checkMovesInDirections(directions, Player, i, j){
+    let moves = [];
+    for (const direction of directions){
+        let ii = i + direction[0];
+        let jj = j + direction[1];
+        while (ii > 0 && jj > 0 && ii < 9 && jj < 9){
+            if (Board[ii][jj].occupyingFigure.type === 'none'){
+                moves.push(Board[ii][jj]);
+            }   else if (Board[ii][jj].occupyingFigure.color !== Player) {
+                moves.push(Board[ii][jj]);
+                break;
+            }   else {
+                break;
+            }
+            ii += direction[0];
+            jj += direction[1];
+        }
+    }
+    return moves;
+}
+
 function knightMoves(Player, i, j){
     let moves = [];
     let directions = [[2, 1], [1, 2], [-1, -2], [-2, -1], [2, -1], [-2, 1], [-1, 2], [1, -2]];
-    for (direction of directions){
+    for (const direction of directions){
         let ii = i + direction[0];
         let jj = j + direction[1];
         if (ii > 0 && jj > 0 && ii < 9 && jj < 9){
@@ -37,75 +58,24 @@ function pawnMoves(Player, i, j){
 }
 
 function rookMoves(Player, i, j){
-    let moves = [];
-    let directions = [[1, 0], [-1, 0], [0, 1],[0, -1]];
-    for (direction of directions){
-        let ii = i + direction[0];
-        let jj = j + direction[1];
-        while (ii > 0 && jj > 0 && ii < 9 && jj < 9){
-            if (Board[ii][jj].occupyingFigure.type === 'none'){
-                moves.push(Board[ii][jj]);
-            }   else if (Board[ii][jj].occupyingFigure.color !== Player) {
-                moves.push(Board[ii][jj]);
-                break;
-            }   else {
-                break;
-            }
-            ii += direction[0];
-            jj += direction[1];
-        }
-    }
-    return moves;
+    const directions = [[1, 0], [-1, 0], [0, 1],[0, -1]];
+    return checkMovesInDirections(directions, Player, i, j);
 }
 
 function bishopMoves(Player, i, j){
-    let moves = [];
-    let directions = [[1, 1], [-1, 1], [-1, -1], [1, -1]];
-    for (direction of directions){
-        let ii = i + direction[0];
-        let jj = j + direction[1];
-        while (ii > 0 && jj > 0 && ii < 9 && jj < 9){
-            if (Board[ii][jj].occupyingFigure.type === 'none'){
-                moves.push(Board[ii][jj]);
-            }   else if (Board[ii][jj].occupyingFigure.color !== Player) {
-                moves.push(Board[ii][jj]);
-                break;
-            }   else {
-                break;
-            }
-            ii += direction[0];
-            jj += direction[1];
-        }
-    }
-    return moves;
+    const directions = [[1, 1], [-1, 1], [-1, -1], [1, -1]];
+    return checkMovesInDirections(directions, Player, i, j);
 }
 
 function queenMoves(Player, i, j){
-    let moves = [];
     let directions = [[1, 1], [-1, 1], [-1, -1], [1, -1], [1, 0], [-1, 0], [0, 1],[0, -1]];
-    for (direction of directions){
-        let ii = i + direction[0];
-        let jj = j + direction[1];
-        while (ii > 0 && jj > 0 && ii < 9 && jj < 9){
-            if (Board[ii][jj].occupyingFigure.type === 'none'){
-                moves.push(Board[ii][jj]);
-            }   else if (Board[ii][jj].occupyingFigure.color !== Player) {
-                moves.push(Board[ii][jj]);
-                break;
-            }   else {
-                break;
-            }
-            ii += direction[0];
-            jj += direction[1];
-        }
-    }
-    return moves;
+    return checkMovesInDirections(directions, Player, i, j);
 }
 
 function kingMoves(Player, i, j){
     let moves = [];
     let directions = [[1, 1], [-1, 1], [-1, -1], [1, -1], [1, 0], [-1, 0], [0, 1],[0, -1]];
-    for (let direction of directions){
+    for (const direction of directions){
         if (!isChecked(Player, i+direction[0], j+direction[1]) && Board[i+direction[0]][j+direction[1]].occupyingFigure.color !== Player){
             moves.push(Board[i+direction[0]][j+direction[1]]);
         }
@@ -115,15 +85,15 @@ function kingMoves(Player, i, j){
 
 function isChecked(Player, i, j){
     if (Player === 'white') {
-        let op = 'black';
-        let opDirection = -1;
+        const op = 'black';
+        const opDirection = -1;
     }
     else {
-        let op = 'white';
-        let opDirection = 1;
+        const op = 'white';
+        const opDirection = 1;
     }
 
-    for (let direction of [[1, 1], [-1, 1], [-1, -1], [1, -1]]) {
+    for (const direction of [[1, 1], [-1, 1], [-1, -1], [1, -1]]) {
         let ii = i + direction[0];
         let jj = j + direction[1];
         while (ii > 0 && jj > 0 && ii < 9 && jj < 9) {
@@ -140,7 +110,7 @@ function isChecked(Player, i, j){
         }
     }
 
-    for (let direction of  [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
+    for (const direction of  [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
         let ii = i + direction[0];
         let jj = j + direction[1];
         while (ii > 0 && jj > 0 && ii < 9 && jj < 9) {
@@ -157,13 +127,13 @@ function isChecked(Player, i, j){
         }
     }
 
-    for (let direction of [[2, 1], [1, 2], [-1, -2], [-2, -1], [2, -1], [-2, 1], [-1, 2], [1, -2]]){
+    for (const direction of [[2, 1], [1, 2], [-1, -2], [-2, -1], [2, -1], [-2, 1], [-1, 2], [1, -2]]){
         if (i+direction[0] < 9 && i+direction[0] > 0 && j+direction[1] > 0 && j+direction[1] < 9 &&
             Board[i+direction[0]][j+direction[1]].occupyingFigure.type === 'knight' &&
             Board[i+direction[0]][j+direction[1]].occupyingFigure.color === op) return true;
     }
 
-    for (let direction of [[-1, opDirection],[1, opDirection]]){
+    for (const direction of [[-1, opDirection],[1, opDirection]]){
             if (Board[i+direction[0]][j+direction[1]].occupyingFigure.type === 'pawn' &&
                 Board[i+direction[0]][j+direction[1]].occupyingFigure.color === op){
                 return true
